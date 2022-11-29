3a968cf05745d055534419c27e981846b4d7bff3
Sazib-Banik <sazib.banik.1@gmail.com>
2022-11-30 02:03:01 +0600

    testimonial part done

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,29 @@ $('.all-service-item').owlCarousel({
         }
     }
 })
+
+$('.all_testimonial_items').owlCarousel({
+    loop:true,
+    margin:10,
+    dots: false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:true
+        },
+        900:{
+            items:1,
+            nav:true
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:true
+        }
+    }
+})
